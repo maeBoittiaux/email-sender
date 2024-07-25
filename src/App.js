506d5ebe165c39fa8d71c1471f22552de22b906a
maebo <maeboittiaux@gmail.com>
2024-07-25 15:48:50 +0100

    Add input field

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <input type='text' className='input-field' placeholder='enter email address ...'></input>
       <button className='submit-button'>submit</button>
     </div>
   );
